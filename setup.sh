a4734a6097e4f60a6cf4050f63f06aa748341e55
Jake Pospischil <moles1@gmail.com>
2018-05-30 10:06:56 +1000

    added instructional text

--- setup.sh
+++ setup.sh
@@ -1,7 +1,46 @@
 ##
+# Intro
+##
+
+echo ""
+echo "Welcome to the RocketPool Smart Node setup wizard!"
+echo "This script will install the following software on this computer:"
+echo ""
+echo "OS Packages:"
+echo "- software-properties-common"
+echo "- build-essential"
+echo "- curl"
+echo "- git"
+echo "- nodejs"
+echo "- your choice of the Geth or Parity ethereum clients"
+echo ""
+echo "NodeJS Packages:"
+echo "- pm2"
+echo "- pm2-server-monit"
+echo ""
+echo "RocketPool Software:"
+echo "- rocketpool contract code repository (https://github.com/rocket-pool/rocketpool.git)"
+echo "- rocketpool smart node daemon repository (https://github.com/rocket-pool/smartnode.git)"
+echo ""
+echo "* pm2 will also be configured to run smart node processes at system startup."
+echo "* RocketPool software repositores will be stored in your home path."
+echo "* Your ethereum client account password will be stored at ~/.smartnode/password with permissions of 'rw-------' (600)."
+echo "  This will be used by the smart node daemon but will never be shared with RocketPool directly."
+echo ""
+echo "A notification email will be sent to RocketPool on completion."
+echo "We will contact you once your node has been registered with the network with instructions to set up server monitoring."
+echo ""
+echo "This script will take several minutes to complete, please be patient."
+echo "Press Control-C at any time to quit."
+echo ""
+
+
+##
 # Parameters
 ##
 
+echo ""
+echo "Please answer the following questions about your Smart Node setup:"
 echo ""
 
 # Node software
@@ -18,7 +57,7 @@ if [ -f ~/.smartnode/password ]; then
 else
     read -p "Please enter your account password: " NODEPASSWORD
     echo "Account password '$NODEPASSWORD' entered."
-    echo "Please record this password somewhere safe for your personal records."; echo ""
+    echo "Please record this password somewhere safe."; echo ""
 fi
 
 # Provider
@@ -54,6 +93,12 @@ fi
 # OS Dependencies
 ##
 
+echo ""
+echo "##########################"
+echo "Installing OS Dependencies"
+echo "##########################"
+echo ""
+
 # Update apt-get
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -68,6 +113,12 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt-get -
 ##
 # Node Software
 ##
+
+echo ""
+echo "##########################"
+echo "Setting Up Ethereum Client"
+echo "##########################"
+echo ""
 
 # Save account password file
 if [ ! -f ~/.smartnode/password ]; then
@@ -94,7 +145,7 @@ case $NODESOFTWARE in
         if [[ $ACCOUNT =~ ([a-fA-F0-9]{40}) ]] ; then
             ACCOUNTADDRESS="0x${BASH_REMATCH[1]}"
         else
-            echo "Failed to get account address, exiting."; echo ""; exit
+            echo "Failed to get new Geth account address, exiting."; echo ""; exit
         fi
 
     ;;
@@ -110,7 +161,7 @@ case $NODESOFTWARE in
         if [[ $ACCOUNT =~ ([a-fA-F0-9]{40}) ]] ; then
             ACCOUNTADDRESS="0x${BASH_REMATCH[1]}"
         else
-            echo "Failed to get account address, exiting."; echo ""; exit
+            echo "Failed to get new Parity account address, exiting."; echo ""; exit
         fi
 
     ;;
@@ -121,6 +172,12 @@ esac
 ##
 # Javascript Dependencies
 ##
+
+echo ""
+echo "##############################"
+echo "Installing NodeJS Dependencies"
+echo "##############################"
+echo ""
 
 # Install NodeJS dependencies
 sudo npm install -g pm2
@@ -136,9 +193,11 @@ if [[ $PM2STARTUP =~ (sudo .*$) ]] ; then
     sudo chown -R ubuntu:ubuntu ~/.pm2
 else
     echo ""
-    echo "Failed to get PM2 startup command."
+    echo "!!!!!!!!!!!!!!!!!!!!"
+    echo "Failed to run PM2 startup command."
     echo "You will need to manually configure PM2 to run at system startup."
     echo "See http://pm2.keymetrics.io/docs/usage/startup/ for more information."
+    echo "!!!!!!!!!!!!!!!!!!!!"
     echo ""
 fi
 
@@ -146,6 +205,12 @@ fi
 ##
 # Clone Repositories
 ##
+
+echo ""
+echo "##############################"
+echo "Installing RocketPool Software"
+echo "##############################"
+echo ""
 
 # RocketPool
 git clone https://github.com/rocket-pool/rocketpool.git ~/rocketpool
@@ -166,39 +231,15 @@ ln -s $HOME/rocketpool/build/contracts contracts
 cd -
 
 
-##
-# Notify
-##
+#################
+## DEVELOPMENT ##
+#################
 
-# Notification post data
-read -r -d '' POSTDATA << EOM
-{
-    "nodeSoftware": "$NODESOFTWARE",
-    "provider": "$PROVIDER",
-    "regionId": "$REGIONID",
-    "subnetId": "$SUBNETID",
-    "instanceId": "$INSTANCEID",
-    "companyName": "$COMPANYNAME",
-    "accountAddress": "$ACCOUNTADDRESS"
-}
-EOM
-
-# Send node setup notification
-RESPONSE="$( curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "$POSTDATA" https://www.rocketpool.net/api/node/notify )"
-if [[ $RESPONSE =~ success ]] ; then
-    echo ""; echo "Successfully emailed node information."; echo ""
-else
-    echo ""
-    echo "Failed to email node information."
-    echo "Node setup notification response: $RESPONSE"
-    echo "Please email dev@rocketpool.net to manually notify us of your node setup."
-    echo ""
-fi
-
-
-################
-## DEVELOPMENT #
-################
+echo ""
+echo "##############################"
+echo "Smart Node Setup (Development)"
+echo "##############################"
+echo ""
 
 # Install truffle and ganache
 sudo npm install -g truffle@v4.1.8
@@ -235,4 +276,53 @@ cd -
 
 # Save running processes
 pm2 save
+
+
+##
+# Notify
+##
+
+echo ""
+echo "####################"
+echo "Sending Notification"
+echo "####################"
+echo ""
+
+# Notification post data
+read -r -d '' POSTDATA << EOM
+{
+    "nodeSoftware": "$NODESOFTWARE",
+    "provider": "$PROVIDER",
+    "regionId": "$REGIONID",
+    "subnetId": "$SUBNETID",
+    "instanceId": "$INSTANCEID",
+    "companyName": "$COMPANYNAME",
+    "accountAddress": "$ACCOUNTADDRESS"
+}
+EOM
+
+# Send node setup notification
+RESPONSE="$( curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "$POSTDATA" https://www.rocketpool.net/api/node/notify )"
+if [[ $RESPONSE =~ success ]] ; then
+    echo ""; echo "Successfully emailed node information."; echo ""
+else
+    echo ""
+    echo "!!!!!!!!!!!!!!!!!!!!"
+    echo "Failed to email node information."
+    echo "Node setup notification response: $RESPONSE"
+    echo "Please email dev@rocketpool.net to manually notify us of your node setup."
+    echo "!!!!!!!!!!!!!!!!!!!!"
+    echo ""
+fi
+
+
+##
+# Cleanup
+##
+
+echo ""
+echo "The RocketPool Smart Node setup wizard is now complete!"
+echo "Your ethereum client account address is: $ACCOUNTADDRESS"
+echo "Please record this somewhere safe. You will need to send funds to this address to cover node operation gas costs."
+echo ""
 
