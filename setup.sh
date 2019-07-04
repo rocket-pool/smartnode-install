9269bc272255610701b8aca1b9b161500f26b4d9
Jake Pospischil <moles1@gmail.com>
2019-07-05 09:13:14 +1000

    updated docker-compose installation

--- setup.sh
+++ setup.sh
@@ -136,7 +136,10 @@ sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent sof
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-compose
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Add user to docker group
 sudo groupadd docker
@@ -163,7 +166,7 @@ echo ""
 
 # Download CLI utility
 sudo curl https://raw.githubusercontent.com/rocket-pool/smartnode-install/master/scripts/rocketpool -o /usr/local/bin/rocketpool
-sudo chmod 755 /usr/local/bin/rocketpool
+sudo chmod +x /usr/local/bin/rocketpool
 
 } &> $OUTPUTTO
 
