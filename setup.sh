0db35344fedc0fd2973bfeafb8969ff3ef36b767
Jake Pospischil <moles1@gmail.com>
2019-07-05 11:54:55 +1000

    run docker config on install

--- setup.sh
+++ setup.sh
@@ -196,6 +196,9 @@ chmod +x "$RP_PATH/docker/setup/pow/start.sh"
 # Download node config
 curl -L "https://github.com/rocket-pool/smartnode-install/releases/download/$GITHUB_RELEASE/node-config.yml" -o "$RP_PATH/config.yml"
 
+# Run docker config
+"$RP_PATH/docker/config.sh"
+
 } &> $OUTPUTTO
 
 
