5c07c38694a737be51989651b901d6e39beabfc7
Jake Pospischil <moles1@gmail.com>
2019-07-08 09:50:39 +1000

    implementing docker config script

--- files/docker-config.sh
+++ files/docker-config.sh
@@ -9,14 +9,30 @@ if [ -z "$RP_PATH" ]; then
     exit 1
 fi
 
+# Choose ethereum client
+echo "Which ethereum client would you like to run?"
+select ETHCLIENT in "Geth" "Parity"; do
+    if [ -z "$ETHCLIENT" ]; then
+        echo "Please select an option with the indicated number."
+    else
+        echo "$ETHCLIENT ethereum client selected."; echo ""; break
+    fi
+done
+
+# Get ethereum client image
+case "$ETHCLIENT" in
+    Geth )   ETHCLIENTIMAGE="ethereum/client-go:latest" ;;
+    Parity ) ETHCLIENTIMAGE="parity/parity:latest" ;;
+esac
+
 # Get docker .env file path
 DOCKERENV="$RP_PATH/docker/.env"
 
 # Write docker config
 echo "COMPOSE_PROJECT_NAME=rocketpool" > "$DOCKERENV"
 echo "" >> "$DOCKERENV"
-echo "POW_CLIENT=geth" >> "$DOCKERENV"
-echo "POW_IMAGE=ethereum/client-go:latest" >> "$DOCKERENV"
+echo "POW_CLIENT=$ETHCLIENT" >> "$DOCKERENV"
+echo "POW_IMAGE=$ETHCLIENTIMAGE" >> "$DOCKERENV"
 echo "POW_NETWORK_ID=77" >> "$DOCKERENV"
 echo "POW_BOOTNODE=enode://6114b79e7928bfb19ff8600bad6e09a49cfc53f7d9513bb0e854566102ee04bac8f494472bcc812211c2cc50684f8a04320d23c17410b52c6175e8246b5a3307@3.216.221.20:30305" >> "$DOCKERENV"
 echo "POW_ETHSTATS_LABEL=RP2Beta-Node" >> "$DOCKERENV"
