99dcb8de4e1bed0503f1525db73dd13cf65518d7
Jake Pospischil <moles1@gmail.com>
2020-09-02 14:12:26 +1000

    adding OS dependencies

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@
 
 
 # The total number of steps in the installation process
-TOTAL_STEPS="99"
+TOTAL_STEPS="7"
 # The Rocket Pool user data path
 RP_PATH="$HOME/.rocketpool"
 # The default smart node package version to download
@@ -95,6 +95,29 @@ case "$PLATFORM" in
 
     # Ubuntu
     Ubuntu)
+
+        # Install OS dependencies
+        progress 1 "Installing OS dependencies..."
+        >&2 sudo apt-get -y update
+        >&2 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+        # Install docker
+        progress 2 "Installing docker..."
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        >&2 sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        >&2 sudo apt-get -y update
+        >&2 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+        # Install docker-compose
+        progress 3 "Installing docker-compose..."
+        >&2 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        >&2 sudo chmod a+rx /usr/local/bin/docker-compose
+
+        # Add user to docker group
+        progress 4 "Adding user to docker group..."
+        >&2 sudo groupadd docker
+        >&2 sudo usermod -aG docker $USER
+
     ;;
 
     # MacOS
@@ -110,21 +133,21 @@ esac
 fi
 
 
-# Create ~/.rocketpool dir
-progress X "Creating Rocket Pool user data directory..."
+# Create ~/.rocketpool dir & files
+progress 5 "Creating Rocket Pool user data directory..."
 >&2 mkdir -p "$RP_PATH/data/validators" || fail "Could not create the Rocket Pool user data directory."
 >&2 touch -a "$RP_PATH/settings.yml" || fail "Could not create the Rocket Pool user settings file."
 
 
 # Download and extract package files
-progress X "Downloading Rocket Pool package files..."
+progress 6 "Downloading Rocket Pool package files..."
 curl -L "$PACKAGE_URL" | tar -xJ -C "$TEMPDIR" || fail "Could not download and extract the Rocket Pool package files."
 >&2 test -d "$PACKAGE_FILES_PATH" || fail "Could not extract the Rocket Pool package files."
 
 
 # Copy package files
-progress X "Copying package files to Rocket Pool user data directory..."
+progress 7 "Copying package files to Rocket Pool user data directory..."
 >&2 test -d "$NETWORK_FILES_PATH" || fail "No package files were found for the selected network."
 >&2 cp -r "$NETWORK_FILES_PATH/"* "$RP_PATH" || fail "Could not copy network package files to the Rocket Pool user data directory."
->&2 find "$RP_PATH" -name "*.sh" -exec chmod +r {} \; || fail "Could not set executable permissions on package files."
+>&2 find "$RP_PATH" -name "*.sh" -exec chmod +x {} \; || fail "Could not set executable permissions on package files."
 
