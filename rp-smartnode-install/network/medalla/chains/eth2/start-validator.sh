98468e7e106647b646b471177f955433143191c3
Jake Pospischil <moles1@gmail.com>
2020-10-13 12:39:54 +1000

    implementing validator graffiti

--- rp-smartnode-install/network/medalla/chains/eth2/start-validator.sh
+++ rp-smartnode-install/network/medalla/chains/eth2/start-validator.sh
@@ -11,9 +11,10 @@ PRYSM_VERSION="v1.0.0-alpha.29"
 # Lighthouse startup
 if [ "$CLIENT" = "lighthouse" ]; then
 
-    GRAFFITI="Rocket Pool $ROCKET_POOL_VERSION / Lighthouse $LIGHTHOUSE_VERSION"
     if [ ! -z "$CUSTOM_GRAFFITI" ]; then
-        GRAFFITI="$GRAFFITI ($CUSTOM_GRAFFITI)"
+        GRAFFITI="$CUSTOM_GRAFFITI"
+    else
+        GRAFFITI="RP $ROCKET_POOL_VERSION / LH $LIGHTHOUSE_VERSION"
     fi
 
     /usr/local/bin/lighthouse validator --testnet medalla --datadir /data/validators/lighthouse --secrets-dir /data/validators/lighthouse/secrets --server "http://$ETH2_PROVIDER" --graffiti "$GRAFFITI"
@@ -24,9 +25,10 @@ fi
 # Prysm startup
 if [ "$CLIENT" = "prysm" ]; then
 
-    GRAFFITI="Rocket Pool $ROCKET_POOL_VERSION / Prysm $PRYSM_VERSION"
     if [ ! -z "$CUSTOM_GRAFFITI" ]; then
-        GRAFFITI="$GRAFFITI ($CUSTOM_GRAFFITI)"
+        GRAFFITI="$CUSTOM_GRAFFITI"
+    else
+        GRAFFITI="RP $ROCKET_POOL_VERSION / PR $PRYSM_VERSION"
     fi
 
     /app/validator/image.binary --wallet-dir /data/validators/prysm --wallet-password-file /data/password --beacon-rpc-provider "$ETH2_PROVIDER" --graffiti "$GRAFFITI"
