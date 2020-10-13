94208681436337206efc25b7f9a3b850279ab9bf
Jake Pospischil <moles1@gmail.com>
2020-10-13 13:39:35 +1000

    implementing validator graffiti

--- rp-smartnode-install/network/medalla/chains/eth2/start-validator.sh
+++ rp-smartnode-install/network/medalla/chains/eth2/start-validator.sh
@@ -2,20 +2,19 @@
 # This script launches ETH2 validator clients for Rocket Pool's docker stack; only edit if you know what you're doing ;)
 
 
-# Client version numbers for graffiti
+# RP version number for graffiti
 ROCKET_POOL_VERSION="v0.0.4"
-LIGHTHOUSE_VERSION="v0.2.13"
-PRYSM_VERSION="v1.0.0-alpha.29"
+
+
+# Get graffiti text
+GRAFFITI="RP $ROCKET_POOL_VERSION"
+if [ ! -z "$CUSTOM_GRAFFITI" ]; then
+    GRAFFITI="$GRAFFITI ($CUSTOM_GRAFFITI)"
+fi
 
 
 # Lighthouse startup
 if [ "$CLIENT" = "lighthouse" ]; then
-
-    if [ ! -z "$CUSTOM_GRAFFITI" ]; then
-        GRAFFITI="$CUSTOM_GRAFFITI"
-    else
-        GRAFFITI="RP $ROCKET_POOL_VERSION / LH $LIGHTHOUSE_VERSION"
-    fi
 
     /usr/local/bin/lighthouse validator --testnet medalla --datadir /data/validators/lighthouse --secrets-dir /data/validators/lighthouse/secrets --server "http://$ETH2_PROVIDER" --graffiti "$GRAFFITI"
 
@@ -24,12 +23,6 @@ fi
 
 # Prysm startup
 if [ "$CLIENT" = "prysm" ]; then
-
-    if [ ! -z "$CUSTOM_GRAFFITI" ]; then
-        GRAFFITI="$CUSTOM_GRAFFITI"
-    else
-        GRAFFITI="RP $ROCKET_POOL_VERSION / PR $PRYSM_VERSION"
-    fi
 
     /app/validator/image.binary --wallet-dir /data/validators/prysm --wallet-password-file /data/password --beacon-rpc-provider "$ETH2_PROVIDER" --graffiti "$GRAFFITI"
 
