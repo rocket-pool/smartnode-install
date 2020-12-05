9e28ed194afd2a92b23593b3f2dffe748567262c
kidkal <kidkal@gmail.com>
2020-12-05 12:30:47 +0000

    fix parameters for lighthouse v1.0.3

--- rp-smartnode-install/network/pyrmont/chains/eth2/start-validator.sh
+++ rp-smartnode-install/network/pyrmont/chains/eth2/start-validator.sh
@@ -16,7 +16,7 @@ fi
 # Lighthouse startup
 if [ "$CLIENT" = "lighthouse" ]; then
 
-    /usr/local/bin/lighthouse validator --testnet pyrmont --datadir /data/validators/lighthouse --init-slashing-protection --delete-lockfiles --beacon-node "http://$ETH2_PROVIDER" --graffiti "$GRAFFITI"
+    /usr/local/bin/lighthouse validator --network pyrmont --datadir /data/validators/lighthouse --init-slashing-protection --delete-lockfiles --beacon-node "http://$ETH2_PROVIDER" --graffiti "$GRAFFITI"
 
 fi
 
