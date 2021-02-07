e36338b9b2d40ed1dfa33c9aae87398b37d6564c
Joe Clapis <jclapis@outlook.com>
2021-02-07 01:35:40 -0500

    Added ETH1_WS_PROVIDER, which points to the geth websocket API (Nimbus only works with web3 websockets, not HTTP)

--- rp-smartnode-install/network/pyrmont/chains/eth2/start-beacon.sh
+++ rp-smartnode-install/network/pyrmont/chains/eth2/start-beacon.sh
@@ -33,6 +33,6 @@ if [ "$CLIENT" = "nimbus" ]; then
     mkdir -p /data/validators/nimbus/validators
     mkdir -p /data/validators/nimbus/secrets
 
-    exec /home/user/nimbus-eth2/build/nimbus_beacon_node --non-interactive --network=pyrmont --data-dir=/ethclient/nimbus --log-file="/ethclient/nimbus/nbc_bn_$(date +"%Y%m%d%H%M%S").log" --web3-url="$ETH1_PROVIDER" --tcp-port=9001 --udp-port=9001 --rpc --rpc-port=5052 --rpc-address=0.0.0.0 --insecure-netkey-password=true --validators-dir=/data/validators/nimbus/validators --secrets-dir=/data/validators/nimbus/secrets --graffiti="$GRAFFITI"
+    exec /home/user/nimbus-eth2/build/nimbus_beacon_node --non-interactive --network=pyrmont --data-dir=/ethclient/nimbus --log-file="/ethclient/nimbus/nbc_bn_$(date +"%Y%m%d%H%M%S").log" --web3-url="$ETH1_WS_PROVIDER" --tcp-port=9001 --udp-port=9001 --rpc --rpc-port=5052 --rpc-address=0.0.0.0 --insecure-netkey-password=true --validators-dir=/data/validators/nimbus/validators --secrets-dir=/data/validators/nimbus/secrets --graffiti="$GRAFFITI"
 
 fi
