a6be94a9ac951c4b81f4690254518ce4c943e2fc
kidkal <kidkal@gmail.com>
2021-02-12 01:28:20 +0000

    comment out example command

--- rp-smartnode-install/network/pyrmont/chains/eth2/restart-validator.sh
+++ rp-smartnode-install/network/pyrmont/chains/eth2/restart-validator.sh
@@ -3,4 +3,4 @@
 # Edit this script to restart ETH2 validator for your custom non-docker set up
 # This file is not used within docker container
 
-systemctl restart lighthousevalidator
+#systemctl restart lighthousevalidator
