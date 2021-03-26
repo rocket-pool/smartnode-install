8b26bb6cb73e5117d391d3788dc21d06e3012466
Joe Clapis <jclapis@outlook.com>
2021-03-25 23:32:23 -0400

    Moved ROCKET_POOL_VERSION to config.yml for easier updating, propagated it to start-beacon.sh and start-validator.sh

--- rp-smartnode-install/network/pyrmont/chains/eth2/start-validator.sh
+++ rp-smartnode-install/network/pyrmont/chains/eth2/start-validator.sh
@@ -2,10 +2,6 @@
 # This script launches ETH2 validator clients for Rocket Pool's docker stack; only edit if you know what you're doing ;)
 
 
-# RP version number for graffiti; MAX 10 chars
-ROCKET_POOL_VERSION="v1.0.0-b.1"
-
-
 # Get graffiti text
 GRAFFITI="RP $ROCKET_POOL_VERSION"
 if [ ! -z "$CUSTOM_GRAFFITI" ]; then
