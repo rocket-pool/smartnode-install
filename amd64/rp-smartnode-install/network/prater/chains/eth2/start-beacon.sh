3fa88f1dccb3488debb6c98a3e0990a2df68a77c
Joe Clapis <jclapis@outlook.com>
2021-06-14 02:17:53 -0400

    Added config files for Prater, fixed a nonstandard port bug

---
+++ amd64/rp-smartnode-install/network/prater/chains/eth2/start-beacon.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# This script launches ETH2 beacon clients for Rocket Pool's docker stack; only edit if you know what you're doing ;)
+
+
+# Get graffiti text
+GRAFFITI="RP $ROCKET_POOL_VERSION"
+if [ ! -z "$CUSTOM_GRAFFITI" ]; then
+    GRAFFITI="$GRAFFITI ($CUSTOM_GRAFFITI)"
+fi
+
+
+# Lighthouse startup
+if [ "$CLIENT" = "lighthouse" ]; then
+
+    CMD="/usr/local/bin/lighthouse beacon --network prater --datadir /ethclient/lighthouse --port $ETH2_P2P_PORT --discovery-port $ETH2_P2P_PORT --eth1 --eth1-endpoints $ETH1_PROVIDER --http --http-address 0.0.0.0 --http-port 5052 --eth1-blocks-per-log-query 150 --disable-upnp"
+
+    if [ ! -z "$ETH2_MAX_PEERS" ]; then
+        CMD="$CMD --target-peers $ETH2_MAX_PEERS"
+    fi
+
+    exec ${CMD}
+
+fi
+
+
+# Nimbus startup
+if [ "$CLIENT" = "nimbus" ]; then
+
+    # Nimbus won't start unless the validator directories already exist
+    mkdir -p /validators/nimbus/validators
+    mkdir -p /validators/nimbus/secrets
+
+    CMD="/home/user/nimbus-eth2/build/nimbus_beacon_node --non-interactive --enr-auto-update --network=prater --data-dir=/ethclient/nimbus --log-file=/ethclient/nimbus/nbc_bn_$(date +%Y%m%d%H%M%S).log --tcp-port=$ETH2_P2P_PORT --udp-port=$ETH2_P2P_PORT --web3-url=$ETH1_WS_PROVIDER --rpc --rpc-address=0.0.0.0 --rpc-port=5052 --insecure-netkey-password=true --validators-dir=/validators/nimbus/validators --secrets-dir=/validators/nimbus/secrets"
+
+    if [ ! -z "$ETH2_MAX_PEERS" ]; then
+        CMD="$CMD --max-peers=$ETH2_MAX_PEERS"
+    fi
+
+    # Graffiti breaks if it's in the CMD string instead of here because of spaces
+    exec ${CMD} --graffiti="$GRAFFITI"
+
+fi
+
+
+# Prysm startup
+if [ "$CLIENT" = "prysm" ]; then
+
+    CMD="/app/cmd/beacon-chain/beacon-chain --accept-terms-of-use --prater --datadir /ethclient/prysm --p2p-tcp-port $ETH2_P2P_PORT --p2p-udp-port $ETH2_P2P_PORT --http-web3provider $ETH1_PROVIDER --rpc-host 0.0.0.0 --rpc-port 5052 --eth1-header-req-limit 150"
+
+    if [ ! -z "$ETH2_MAX_PEERS" ]; then
+        CMD="$CMD --p2p-max-peers $ETH2_MAX_PEERS"
+    fi
+
+    exec ${CMD}
+
+fi
+
+
+# Teku startup
+if [ "$CLIENT" = "teku" ]; then
+
+    CMD="/opt/teku/bin/teku --network=prater --data-path=/ethclient/teku --p2p-port=$ETH2_P2P_PORT --eth1-endpoint=$ETH1_PROVIDER --rest-api-enabled --rest-api-interface=0.0.0.0 --rest-api-port=5052 --rest-api-host-allowlist=* --eth1-deposit-contract-max-request-size=150"
+
+    if [ ! -z "$ETH2_MAX_PEERS" ]; then
+        CMD="$CMD --p2p-peer-lower-bound=$ETH2_MAX_PEERS --p2p-peer-upper-bound=$ETH2_MAX_PEERS"
+    fi
+
+    exec ${CMD}
+
+fi
---
+++ arm64/rp-smartnode-install/network/prater/chains/eth1/start-node.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# This script launches ETH1 clients for Rocket Pool's docker stack; only edit if you know what you're doing ;)
+
+
+# Geth startup
+if [ "$CLIENT" = "geth" ]; then
+
+    CMD="/usr/local/bin/geth --goerli --datadir /ethclient/geth --http --http.addr 0.0.0.0 --http.port 8545 --http.api eth,net,personal,web3 --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api eth,net,personal,web3"
+
+    if [ ! -z "$ETHSTATS_LABEL" ] && [ ! -z "$ETHSTATS_LOGIN" ]; then
+        CMD="$CMD --ethstats $ETHSTATS_LABEL:$ETHSTATS_LOGIN"
+    fi
+
+    if [ ! -z "$GETH_CACHE_SIZE" ]; then
+        CMD="$CMD --cache $GETH_CACHE_SIZE"
+    fi
+
+    if [ ! -z "$GETH_MAX_PEERS" ]; then
+        CMD="$CMD --maxpeers $GETH_MAX_PEERS"
+    fi
+
+    if [ ! -z "$ETH1_P2P_PORT" ]; then
+        CMD="$CMD --port $ETH1_P2P_PORT"
+    fi
+
+    exec ${CMD} --http.vhosts '*'
+
+fi
+
+
+# Infura startup
+if [ "$CLIENT" = "infura" ]; then
+
+    exec /go/bin/rocketpool-pow-proxy --httpPort 8545 --wsPort 8546 --network goerli --projectId $INFURA_PROJECT_ID --providerType infura
+
+fi
+
+
+# Pocket startup
+if [ "$CLIENT" = "pocket" ]; then
+
+    exec /go/bin/rocketpool-pow-proxy --httpPort 8545 --network eth-goerli --projectId $POCKET_PROJECT_ID --providerType pocket
+
+fi
+
+
+# Custom provider startup
+if [ "$CLIENT" = "custom" ]; then
+
+    exec /go/bin/rocketpool-pow-proxy --httpPort 8545 --wsPort 8546 --httpProviderUrl $HTTP_PROVIDER_URL --wsProviderUrl $WS_PROVIDER_URL
+
+fi
+
