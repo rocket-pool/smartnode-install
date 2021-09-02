bdc6de45de2a6ae40a86a7e17aa87759f9f87bab
Joe Clapis <jclapis@outlook.com>
2021-09-02 01:24:02 -0400

    Changed Ubuntu to install chrony instead of NTP

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ case "$PLATFORM" in
         # Install OS dependencies
         progress 1 "Installing OS dependencies..."
         { sudo apt-get -y update || fail "Could not update OS package definitions."; } >&2
-        { sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common ntp || fail "Could not install OS packages."; } >&2
+        { sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common chrony || fail "Could not install OS packages."; } >&2
 
         # Install docker
         progress 2 "Installing docker..."
