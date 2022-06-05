117494ab1fe4c294410606a5342635bd7629a1ad
Joe Clapis <jclapis@outlook.com>
2022-06-05 03:00:32 -0400

    Updated Ropsten TTDs

--- install/scripts/start-bn.sh
+++ install/scripts/start-bn.sh
@@ -64,7 +64,7 @@ if [ "$CC_CLIENT" = "lighthouse" ]; then
     if [ "$NETWORK" = "kiln" ]; then
         CMD="$CMD --terminal-total-difficulty-override=20000000000000 --boot-nodes=enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk"
     elif [ "$NETWORK" = "ropsten" ]; then
-        CMD="$CMD --terminal-total-difficulty-override=100000000000000000000000"
+        CMD="$CMD --terminal-total-difficulty-override=50000000000000000"
     fi
 
     if [ ! -z "$BN_MAX_PEERS" ]; then
@@ -121,7 +121,7 @@ if [ "$CC_CLIENT" = "nimbus" ]; then
     CMD="$PERF_PREFIX /home/user/nimbus-eth2/build/nimbus_beacon_node --non-interactive --enr-auto-update --network=$NIMBUS_NETWORK --data-dir=/ethclient/nimbus --tcp-port=$BN_P2P_PORT --udp-port=$BN_P2P_PORT $ETH1_PROVIDER_ARG --rest --rest-address=0.0.0.0 --rest-port=${BN_API_PORT:-5052} --insecure-netkey-password=true --validators-dir=/validators/nimbus/validators --secrets-dir=/validators/nimbus/secrets --doppelganger-detection=$DOPPELGANGER_DETECTION --jwt-secret=/secrets/jwtsecret --suggested-fee-recipient=$(cat /validators/nimbus/$FEE_RECIPIENT_FILE) $BN_ADDITIONAL_FLAGS"
 
     if [ "$NETWORK" = "ropsten" ]; then
-        CMD="$CMD --terminal-total-difficulty-override=100000000000000000000000"
+        CMD="$CMD --terminal-total-difficulty-override=50000000000000000"
     fi
 
     if [ ! -z "$BN_MAX_PEERS" ]; then
@@ -195,7 +195,7 @@ if [ "$CC_CLIENT" = "teku" ]; then
     CMD="$PERF_PREFIX /opt/teku/bin/teku --network=$TEKU_NETWORK --data-path=/ethclient/teku --p2p-port=$BN_P2P_PORT --ee-endpoint=$EC_HTTP_ENDPOINT --rest-api-enabled --rest-api-interface=0.0.0.0 --rest-api-port=${BN_API_PORT:-5052} --rest-api-host-allowlist=* --eth1-deposit-contract-max-request-size=150 --log-destination=CONSOLE --ee-jwt-secret-file=/secrets/jwtsecret $BN_ADDITIONAL_FLAGS"
 
     if [ "$NETWORK" = "ropsten" ]; then
-        CMD="$CMD --Xnetwork-total-terminal-difficulty-override=100000000000000000000000"
+        CMD="$CMD --Xnetwork-total-terminal-difficulty-override=50000000000000000"
     fi
 
     if [ ! -z "$BN_MAX_PEERS" ]; then
