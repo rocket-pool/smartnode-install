9918ca65352d77a343197845f9de7bfc510004b6
Joe Clapis <jclapis@outlook.com>
2022-08-11 01:19:28 -0400

    Added a script for MEV-boost to handle network renaming

---
+++ install/scripts/start-mev-boost.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Set up the network-based flag
+if [ "$NETWORK" = "mainnet" ]; then
+    MEV_NETWORK="mainnet"
+elif [ "$NETWORK" = "prater" ]; then
+    MEV_NETWORK="goerli"
+elif [ "$NETWORK" = "kiln" ]; then
+    MEV_NETWORK="kiln"
+elif [ "$NETWORK" = "ropsten" ]; then
+    MEV_NETWORK="ropsten"
+else
+    echo "Unknown network [$NETWORK]"
+    exit 1
+fi
+
+# Run MEV-boost
+exec /app/mev-boost ${MEV_NETWORK} -addr 0.0.0.0:${MEV_BOOST_PORT} -relay-check -relays ${MEV_BOOST_RELAYS}
