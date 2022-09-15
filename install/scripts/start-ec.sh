e3ad18857013bb4ec9bbcaac9423ef8758253881
Joe Clapis <jclapis@outlook.com>
2022-09-15 05:19:31 -0400

    Added WS to Besu, switched Nimbus to WS on the Engine API

--- install/scripts/start-ec.sh
+++ install/scripts/start-ec.sh
@@ -189,7 +189,7 @@ if [ "$CLIENT" = "besu" ]; then
         openssl rand -hex 32 | tr -d "\n" > /secrets/jwtsecret
     fi
 
-    CMD="$PERF_PREFIX /opt/besu/bin/besu --network=$BESU_NETWORK --data-path=/ethclient/besu --rpc-http-enabled --rpc-http-host=0.0.0.0 --rpc-http-port=${EC_HTTP_PORT:-8545} --host-allowlist=* --revert-reason-enabled --rpc-http-max-active-connections=65536 --data-storage-format=bonsai --sync-mode=X_CHECKPOINT --fast-sync-min-peers=3 --nat-method=docker --p2p-host=$EXTERNAL_IP --Xmerge-support --engine-rpc-enabled --engine-rpc-port=${EC_ENGINE_PORT:-8551} --engine-host-allowlist=* --engine-jwt-secret=/secrets/jwtsecret $EC_ADDITIONAL_FLAGS"
+    CMD="$PERF_PREFIX /opt/besu/bin/besu --network=$BESU_NETWORK --data-path=/ethclient/besu --rpc-http-enabled --rpc-http-host=0.0.0.0 --rpc-http-port=${EC_HTTP_PORT:-8545} --rpc-ws-enabled --rpc-ws-host=0.0.0.0 --rpc-ws-port=${EC_WS_PORT:-8546} --host-allowlist=* --revert-reason-enabled --rpc-http-max-active-connections=65536 --data-storage-format=bonsai --sync-mode=X_CHECKPOINT --fast-sync-min-peers=3 --nat-method=docker --p2p-host=$EXTERNAL_IP --Xmerge-support --engine-rpc-enabled --engine-rpc-port=${EC_ENGINE_PORT:-8551} --engine-host-allowlist=* --engine-jwt-secret=/secrets/jwtsecret $EC_ADDITIONAL_FLAGS"
 
     if [ ! -z "$TTD_OVERRIDE" ]; then
         CMD="$CMD --override-genesis-config=terminalTotalDifficulty=$TTD_OVERRIDE"
