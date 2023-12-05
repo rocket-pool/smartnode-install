f43e52711cec7439ae44fa60dbce31a4b7e8a94b
Fornax <23104993+fornax2@users.noreply.github.com>
2023-12-04 22:05:44 -0300

    Fix cli format

--- install/scripts/start-bn.sh
+++ install/scripts/start-bn.sh
@@ -78,8 +78,8 @@ if [ "$CC_CLIENT" = "lighthouse" ]; then
         --http-allow-sync-stalled \
         --execution-jwt=/secrets/jwtsecret \
         --always-prefer-builder-payload \
-        --historic-state-cache-size 2 \ 
-        --quic-port ${BN_P2P_QUIC_PORT:-9002}
+        --quic-port ${BN_P2P_QUIC_PORT:-9002} \
+        --historic-state-cache-size 2 \
         $BN_ADDITIONAL_FLAGS"
 
     # Performance tuning for ARM systems
