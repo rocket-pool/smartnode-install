ce766a3b844c813a947c712ddb805979ac73f333
Fornax <23104993+0xfornax@users.noreply.github.com>
2024-12-12 11:54:27 -0300

    Remove additional flags parsing

--- install/scripts/start-mev-boost.sh
+++ install/scripts/start-mev-boost.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-parse_additional_flags() {
-  # Initialize an empty string for additional arguments
-  ADDITIONAL_ARGS=""
-
-  # Check if the environment variable MEV_BOOST_ADDITIONAL_FLAGS is not empty
-  if [ -n "$MEV_BOOST_ADDITIONAL_FLAGS" ]; then
-    # Split the input string into an array of key-value pairs using comma as the delimiter
-    IFS=',' read -r -a pairs <<EOF
-$MEV_BOOST_ADDITIONAL_FLAGS
-EOF
-
-    # Iterate over each key-value pair
-    for pair in "${pairs[@]}"; do
-      # Extract the key and value from the current pair
-      key=$(echo "$pair" | cut -d'=' -f1)
-      value=$(echo "$pair" | cut -d'=' -f2)
-
-      # Append the key-value pair to the ADDITIONAL_ARGS string
-      ADDITIONAL_ARGS="$ADDITIONAL_ARGS -${key} ${value}"
-    done
-  fi
-}
-
 # Set up the network-based flag
 if [ "$NETWORK" = "mainnet" ]; then
     MEV_NETWORK="mainnet"
@@ -35,6 +12,4 @@ else
     exit 1
 fi
 
-parse_additional_flags
-
-exec /app/mev-boost -${MEV_NETWORK} -addr 0.0.0.0:${MEV_BOOST_PORT} -relay-check -relays ${MEV_BOOST_RELAYS} ${ADDITIONAL_ARGS}
+exec /app/mev-boost -${MEV_NETWORK} -addr 0.0.0.0:${MEV_BOOST_PORT} -relay-check -relays ${MEV_BOOST_RELAYS} ${$MEV_BOOST_ADDITIONAL_FLAGS}
