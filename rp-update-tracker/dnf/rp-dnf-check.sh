4f6d0c95223ce8a44a7720b46992ee71e3878e44
Jacob Shufro <jacob@shuf.ro>
2025-05-13 12:58:36 -0400

    Remove files that were moved into smartnode.git

--- rp-update-tracker/dnf/rp-dnf-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/usr/share/dnf-metrics.sh | sponge /var/lib/node_exporter/textfile_collector/dnf.prom || true
-/usr/share/rp-version-check.sh | sponge /var/lib/node_exporter/textfile_collector/rp.prom || true
